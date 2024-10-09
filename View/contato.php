d51b6ea6117cd127e15e60799029ce6b7c7845d4
Iury Raminelli <raminelliiury4@gmail.com>
2024-10-09 12:58:09 -0300

    Arrumando o footer e header

--- View/contato.php
+++ View/contato.php
@@ -19,152 +19,9 @@
 </head>
 
 <body>
-<section class="section-inicio">
-    <header class="header-inicio">
-      <nav class="navbar navbar-expand-lg navbar-dark">
-        <div class="container">
-          <a class="navbar-brand" href="index.php"><img src="img/logoo-ads.png" alt="Logo" width="50" height="50" class="d-inline-block align-text-top"></a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse justify-content-between" id="navbarNav">
-            <ul class="navbar-nav mx-auto">
-              <?php
-              if (session_status() == PHP_SESSION_NONE) {
-                session_start();
-              }
-              if (isset($_SESSION["USER_LOGIN"]) && $_SESSION["USER_LOGIN"] == "administrador@teste.com") :
-              ?>
-                <li class="nav-item"><a class="nav-link" href="sobre.php">Sobre</a></l>
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Navegar
-                  </a>
-                  <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="cadastrarPedidos.php">Data</a></li>
-                    <li><a class="dropdown-item" href="#">Autores</a></li>
-                    <li><a class="dropdown-item" href="#">Título</a></li>
-                    <li><a class="dropdown-item" href="#">Assunto</a></li>
-                    <li><a class="dropdown-item" href="#">Tipo</a></li>
-                  </ul>
-                </li>
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Documentos
-                  </a>
-                  <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Estatuto</a></li>
-                    <li><a class="dropdown-item" href="#">Atas</a></li>
-                    <li><a class="dropdown-item" href="#">algo2</a></li>
-                    <li><a class="dropdown-item" href="#">algo3</a></li>
-                    <li><a class="dropdown-item" href="#">algo4</a></li>
-                  </ul>
-                </li>
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    ControleADM
-                  </a>
-                  <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Estatuto</a></li>
-                    <li><a class="dropdown-item" href="#">Membros</a></li>
-                    <li><a class="dropdown-item" href="#">Financeiro</a></li>
-                    <li><a class="dropdown-item" href="#">Atividades</a></li>
-                    <li><a class="dropdown-item" href="#">Atas</a></li>
-                  </ul>
-                </li>
-
-                <li class="nav-item"><a class="nav-link" href="contato.php">Contato</a></li>
-              <?php endif; ?>
-
-              <?php if (isset($_SESSION["USER_LOGIN"]) && $_SESSION["USER_LOGIN"] != "administrador@teste.com") : ?>
-                <li class="nav-item"><a class="nav-link" href="sobre.php">Sobre</a></li>
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Navegar
-                  </a>
-                  <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="cadastrarPedidos.php">Data</a></li>
-                    <li><a class="dropdown-item" href="#">Autores</a></li>
-                    <li><a class="dropdown-item" href="#">Título</a></li>
-                    <li><a class="dropdown-item" href="#">Assunto</a></li>
-                    <li><a class="dropdown-item" href="#">Tipo</a></li>
-                  </ul>
-                </li>
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Documentos
-                  </a>
-                  <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="cadastrarPedidos.php">algo0</a></li>
-                    <li><a class="dropdown-item" href="#">algo1</a></li>
-                    <li><a class="dropdown-item" href="#">algo2</a></li>
-                    <li><a class="dropdown-item" href="#">algo3</a></li>
-                    <li><a class="dropdown-item" href="#">algo4</a></li>
-                  </ul>
-                </li>
-
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    ControleUSER
-                  </a>
-                  <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Atividades</a></li>
-                    <li><a class="dropdown-item" href="#">Atas</a></li>
-                  </ul>
-                </li>
-
-                <li class="nav-item"><a class="nav-link" href="contato.php">Contato</a></li>
-              <?php endif; ?>
-
-              <?php if (!isset($_SESSION["USER_LOGIN"])) : ?>
-                <li class="nav-item"><a class="nav-link" href="sobre.php">Sobre</a></li>
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Navegar
-                  </a>
-                  <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="cadastrarPedidos.php">Data</a></li>
-                    <li><a class="dropdown-item" href="#">Autores</a></li>
-                    <li><a class="dropdown-item" href="#">Título</a></li>
-                    <li><a class="dropdown-item" href="#">Assunto</a></li>
-                    <li><a class="dropdown-item" href="#">Tipo</a></li>
-                  </ul>
-                </li>
-                <li class="nav-item"><a class="nav-link" href="contato.php">Contato</a></li>
-              <?php endif; ?>
-            </ul>
-            <ul class="navbar-nav">
-              <?php if (!isset($_SESSION["USER_LOGIN"])) : ?>
-                <div class="dropdown text">
-                  <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                    <img src="img/perfil2.webp" alt="mdo" width="32" height="32" class="rounded-circle">
-                  </a>
-                  <ul class="dropdown-menu text-small">
-                    <li><a class="dropdown-item" href="login.php">Entrar</a></li>
-                    <li><a class="dropdown-item" href="cadastro.php">Cadastrar</a></li>
-                  </ul>
-                </div>
-              <?php else : ?>
-                <div class="dropdown text">
-                  <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                    <img src="img/perfil2.webp" alt="mdo" width="32" height="32" class="rounded-circle">
-                  </a>
-                  <ul class="dropdown-menu text-small">
-                    <li><a class="dropdown-item" href="#">Configurações</a></li>
-                    <li><a class="dropdown-item" href="perfil.php">Perfil</a></li>
-                    <li>
-                      <hr class="dropdown-divider">
-                    </li>
-                    <li><a class="dropdown-item" href="sair.php">Sair</a></li>
-                  </ul>
-                </div>
-              <?php endif; ?>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </header>
-  </section>
+<?php
+        include_once 'header.php';
+      ?>
 
   <div class="container">
 
@@ -200,9 +57,9 @@
       </form>
     </div>
 
-    <footer class="pt-5 my-5 text-body-secondary border-top">
-    Desenvolvido por <a href="https://instagram.com/iury_raminelli">Iury Raminelli</a> - ADS18 &middot; &copy; 2024
-    </footer>
+    <?php
+        include_once 'footer.php';
+      ?>
   </div>
 </body>
 
---
+++ View/footer.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <footer class="pt-5 my-5 text-body-secondary border-top">
+        Desenvolvido por <a href="https://instagram.com/iury_raminelli">Iury Raminelli</a> - ADS18 &middot; &copy; 2024
+    </footer>
+</body>
+</html>
