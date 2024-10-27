1e8f999ad7e1770c7b1a5b01b1ffd2c5fc44090c
Iury Raminelli <raminelliiury4@gmail.com>
2024-10-27 17:32:42 -0300

    esqueci do banco

--- src/Conexao/tcc.sql
+++ src/Conexao/tcc.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 25/10/2024 às 14:25
+-- Tempo de geração: 27/10/2024 às 21:32
 -- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
+-- Versão do PHP: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -45,6 +45,51 @@ INSERT INTO `atas` (`id`, `dia`, `descricao`, `arquivo`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estrutura para tabela `atividade`
+--
+
+CREATE TABLE `atividade` (
+  `id` int(11) NOT NULL,
+  `imagem` varchar(255) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `descricao` varchar(255) NOT NULL,
+  `dia` date NOT NULL,
+  `local` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `atividade`
+--
+
+INSERT INTO `atividade` (`id`, `imagem`, `nome`, `descricao`, `dia`, `local`) VALUES
+(1, 'src/View/img/3e3166157ec5aab796cadcbc4de31351.jpg', '16° SeTEIC', 'Semana Academica foda-se', '2024-10-27', 'IFFar-SVS'),
+(2, 'src/View/img/logo-ads.png', 'Teste', 'teste2.0', '2024-10-30', 'Segredo'),
+(3, 'src/View/img/pdf.png', 'teste2', 'teste2', '2024-10-06', 'São Vicente do Sul'),
+(4, 'src/View/img/PHOTO-2024-08-31-09-31-52.jpg', 'CodeRace', 'ADS participando do evento da CodeRace da AMF.', '2024-08-21', 'Faculdade AMF');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `estatuto`
+--
+
+CREATE TABLE `estatuto` (
+  `id` int(11) NOT NULL,
+  `ano` year(4) NOT NULL,
+  `descricao` varchar(255) NOT NULL,
+  `arquivo` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `estatuto`
+--
+
+INSERT INTO `estatuto` (`id`, `ano`, `descricao`, `arquivo`) VALUES
+(1, '2024', 'teste', 'src/View/img/Ata_de_Posse_do_Centro_Acadêmico.docx');
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura para tabela `transacao`
 --
 
@@ -62,7 +107,8 @@ CREATE TABLE `transacao` (
 INSERT INTO `transacao` (`id`, `quantidade`, `dia`, `descricao`) VALUES
 (1, 555, '2024-10-25', 'Patrocínio AMPER'),
 (2, 200, '0000-00-00', 'Patrocínio Sicredi'),
-(3, -900, '2024-10-22', 'Pagamento Águas');
+(3, -900, '2024-10-22', 'Pagamento Águas'),
+(4, 545, '2024-10-26', 'Teste');
 
 -- --------------------------------------------------------
 
@@ -97,6 +143,18 @@ ALTER TABLE `atas`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Índices de tabela `atividade`
+--
+ALTER TABLE `atividade`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `estatuto`
+--
+ALTER TABLE `estatuto`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Índices de tabela `transacao`
 --
 ALTER TABLE `transacao`
@@ -120,10 +178,22 @@ ALTER TABLE `atas`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT de tabela `atividade`
+--
+ALTER TABLE `atividade`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de tabela `estatuto`
+--
+ALTER TABLE `estatuto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT de tabela `transacao`
 --
 ALTER TABLE `transacao`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de tabela `user`
